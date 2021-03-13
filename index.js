c7058b9a854e2cc98803a4f5903cee320c6d1ff4
Zac Alexander <zac@znacreative.com>
2021-03-13 01:28:32 +0000

    Transaction tracking and validation

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 // Allow multiple accounts to be created
 // Each account can have many transactions
 // Allow withdrawals and deposits into accounts
-//? Allow us to retrieve the transaction history of an account (all withdrawals and deposits)
+// Allow us to retrieve the transaction history of an account (all withdrawals and deposits)
 // Allow us to retrieve the current balance of the account at any time
-//? Don't allow withdrawals that exceed the remaining balance of the account
-
-// let balance = 500.00; // No longer needed
+// Don't allow withdrawals that exceed the remaining balance of the account
 
 class Account {
   constructor(username) {
     this.username = username;
-    // Account balance starts at zero
-    this.balance = 0;
+    this.transactions = [];
+  }
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
+  }
+  addTransactions(transaction) {
+    this.transactions.push(transaction);
   }
 }
 
@@ -21,7 +28,13 @@ class Transactions {
     this.account = account;
   }
   commit() {
-    this.account.balance += this.value;
+    // Check if account has enough to withdrawal
+    if (!this.isAllowed()) return false;
+    // Transaction timestamp
+    this.time = new Date();
+    // Add transactions history to account
+    this.account.addTransactions(this);
+    return true;
   }
 }
 
@@ -29,29 +42,37 @@ class Withdrawal extends Transactions {
   get value() {
     return - this.amount;
   }
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
 }
 
 class Deposit extends Transactions {
   get value() {
     return this.amount;
   }
+  isAllowed() {
+    return true;
+  }
 }
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+// DRIVER CODE
 const myAccount = new Account('snow-patrol');
-console.log("Starting Balance: ", myAccount.balance);
-t1 = new Withdrawal(50.25, myAccount);
-t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Account Balance: ', myAccount.balance); // Starting balance
 
-t2 = new Withdrawal(9.99, myAccount);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Attempting to withdraw...'); // Expected to fail
+const t1 = new Withdrawal(5.00, myAccount);
+console.log('Withdrawal result:', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-t3 = new Deposit(120.00, myAccount);
-t3.commit();
-console.log('Transaction 3:', t3);
+console.log('Processing deposit...');
+const t2 = new Deposit(9.99, myAccount);
+console.log('Deposit result:', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
 
+console.log('Processing withdrawal for 9.99...');
+const t3 = new Withdrawal(9.99, myAccount);
+console.log('Withdrawal result:', t3.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-console.log('Final Balance: ', myAccount.balance);
+console.log('Account Transaction History: ', myAccount.transactions);
