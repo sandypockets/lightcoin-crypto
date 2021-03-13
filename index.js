eaee5db8c7f567419e5e571caa426c004c618266
Zac Alexander <zac@znacreative.com>
2021-03-13 00:55:30 +0000

    Removed unnecessary methods now included in superclass

--- index.js
+++ index.js
@@ -20,24 +20,27 @@ class Transactions {
     this.amount = amount;
     this.account = account;
   }
+  commit() {
+    this.account.balance += this.value;
+  }
 }
 
 class Withdrawal extends Transactions {
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return - this.amount;
   }
 }
 
 class Deposit extends Transactions {
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 }
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 const myAccount = new Account('snow-patrol');
-
+console.log("Starting Balance: ", myAccount.balance);
 t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
@@ -51,4 +54,4 @@ t3.commit();
 console.log('Transaction 3:', t3);
 
 
-console.log('Balance:', myAccount.balance);
+console.log('Final Balance: ', myAccount.balance);
